e651f704fd4657a04010ab90fdf6e85272b46550
Jose.Calderon <jose.calderon@tranzact.net>
2021-12-09 01:41:21 -0500

    mensaje final encuesta

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   let mostrarEncuestas: boolean = true;
   let cargandoi: boolean = true;
   let respondiendo: boolean = false;
+  let encuestaTerminada: boolean = false;
   let evaluaciones = [];
   let campana: any = {};
   let preguntas = [];
@@ -49,6 +50,7 @@
     if (evaluaciones.some((t) => t.preguntas.some((p) => !p.valor))) {
       console.log("-");
     } else {
+      respondiendo = true;
       const dataFinaliza = { ev: evaluador.id };
       const respfinaliza = await fetch(`/api/private/campana/${campana.id}`, {
         method: "PUT",
@@ -57,6 +59,8 @@
         },
         body: JSON.stringify(dataFinaliza),
       });
+      encuestaTerminada = true;
+      mostrarEncuestas = true;
     }
   }
 </script>
@@ -201,3 +205,27 @@
     </div>
   {/if}
 </div>
+
+{#if encuestaTerminada}
+  <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" />
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+      <div
+        class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+      >
+        <div>
+          <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+            <!-- Heroicon name: outline/check -->
+            <svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
+          <div class="mt-3 text-center sm:mt-5">
+            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Gracias por completar las encuestas</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+{/if}
